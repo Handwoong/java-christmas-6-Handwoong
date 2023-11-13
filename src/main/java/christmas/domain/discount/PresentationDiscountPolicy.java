73b4452250f55a5e82840c3c4774649551a46adc
Handwoong <handwoong@gmail.com>
2023-11-13 16:49:16 +0900

    feat(discount): 증정 할인 정책 구현

---
+++ src/main/java/christmas/domain/discount/PresentationDiscountPolicy.java
@@ -0,0 +1,22 @@
+package christmas.domain.discount;
+
+import christmas.domain.order.Order;
+
+public class PresentationDiscountPolicy implements DiscountPolicy {
+    public static final String PRESENTATION_MENU_NAME = "샴페인";
+    private static final int PRESENTATION_MENU_PRICE = 25_000;
+    private static final int EVENT_ORDER_PRICE = 120_000;
+
+    @Override
+    public boolean support(final int date, final EventCalender calender, final Order order) {
+        return order.isTotalPriceGreaterThanOrEqual(EVENT_ORDER_PRICE);
+    }
+
+    @Override
+    public int discount(final int date, final EventCalender calender, final Order order) {
+        if (support(date, calender, order)) {
+            return PRESENTATION_MENU_PRICE;
+        }
+        return 0;
+    }
+}
