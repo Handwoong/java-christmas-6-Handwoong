024d83fef173e699e46b316f5c523e0f2d7d47cf
Handwoong <handwoong@gmail.com>
2023-11-13 15:15:51 +0900

    feat(discount): 주말 할인 정책 구현

---
+++ src/main/java/christmas/domain/discount/WeekendDiscountPolicy.java
@@ -0,0 +1,29 @@
+package christmas.domain.discount;
+
+import static christmas.domain.discount.ChristmasEventCalender.FRIDAY;
+import static christmas.domain.discount.ChristmasEventCalender.SATURDAY;
+
+import christmas.domain.order.Order;
+
+public class WeekendDiscountPolicy implements DiscountPolicy {
+    private static final String DISCOUNT_CATEGORY = "메인";
+    private static final int DISCOUNT_AMOUNT = 2_023;
+
+    @Override
+    public boolean support(final int date, final EventCalender calender, final Order order) {
+        return (order.isTotalPriceGreaterThanOrEqual(MIN_ORDER_PRICE)) &&
+                (calender.isEventDayOfWeek(date, FRIDAY, SATURDAY));
+    }
+
+    @Override
+    public int discount(final int date, final EventCalender calender, final Order order) {
+        if (support(date, calender, order)) {
+            return calculateDiscountAmount(order);
+        }
+        return 0;
+    }
+
+    private int calculateDiscountAmount(final Order order) {
+        return DISCOUNT_AMOUNT * (order.calculateCategoryMenuQuantity(DISCOUNT_CATEGORY));
+    }
+}
